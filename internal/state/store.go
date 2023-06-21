e766df2b081787bb1e6174f12ea25c2425e2a39c
Edward Ciccone <edward@Edwards-MacBook-Pro.local>
2023-06-21 09:45:17 -0500

    implemented func to insert state Zone into database

--- internal/state/store.go
+++ internal/state/store.go
@@ -3,6 +3,7 @@ package state
 import (
 	"context"
 	"database/sql"
+	"fmt"
 )
 
 type Store struct {
@@ -20,4 +21,33 @@ func (s *Store) SelectEntity(ctx context.Context, stateID string) (Entity, error
 
 func (s *Store) InsertEntity(ctx context.Context, state Entity) (sql.Result, error) {
 	return state.Insert(ctx, s.DB)
+}
+
+func (s *Store) InsertZone(ctx context.Context, zone Zone) error {
+	zoneEntity := zone.ToEntity()
+	if err := zoneEntity.Insert(ctx, s.DB); err != nil {
+		return fmt.Errorf("failed to insert ZoneEntity into database: %w", err)
+	}
+
+	for _, polygon := range zone.Geometry {
+		perimeterEntity := PerimeterEntity{
+			ZoneID: zoneEntity.ID,
+			Points: polygon.Permiter(),
+		}
+		if err := perimeterEntity.Insert(ctx, s.DB); err != nil {
+			return fmt.Errorf("failed to insert PerimeterEntity into database: %w", err)
+		}
+
+		for _, hole := range polygon.Holes() {
+			holeEntity := HoleEntity{
+				PerimieterID: perimeterEntity.ID,
+				Points:       hole,
+			}
+			if _, err := holeEntity.Insert(ctx, s.DB); err != nil {
+				return fmt.Errorf("failed to insert HoleEntity into database: %w", err)
+			}
+		}
+	}
+
+	return nil
 }
