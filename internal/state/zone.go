4a81bba1b0bf2859ae739a031e811be6eba39bc8
Edward Ciccone <edward@Edwards-MacBook-Pro.local>
2023-06-24 14:54:51 -0500

    moved perimeter and hole entities to geometry.go file

--- internal/state/zone.go
+++ internal/state/zone.go
@@ -104,33 +104,3 @@ func (z ZoneEntityURIMap) Select(ctx context.Context, db *sql.DB, state string) 
 
 	return nil
 }
-
-type PerimeterEntity struct {
-	ID     int
-	ZoneID int
-	Points geometry.PointCollection
-}
-
-func (p *PerimeterEntity) Insert(ctx context.Context, db QueryRower) error {
-	query := `
-		INSERT INTO state_zone_perimeters(sz_id, boundary)
-		VALUES($1, $2)
-		RETURNING id`
-
-	return db.QueryRowContext(ctx, query,
-		p.ZoneID,
-		p.Points.String()).Scan(&p.ID)
-}
-
-type HoleEntity struct {
-	PerimieterID int
-	Points       geometry.PointCollection
-}
-
-func (h *HoleEntity) Insert(ctx context.Context, db Execer) (sql.Result, error) {
-	query := `
-		INSERT INTO state_zone_holes(zp_id, boundary)
-		VALUES($1, $2)`
-
-	return db.ExecContext(ctx, query, h.PerimieterID, h.Points.String())
-}
